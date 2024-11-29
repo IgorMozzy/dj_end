e586b262f249eb86bdda9c7992bf9b440be12386
sqfav <@>
2024-11-29 10:16:29 +0100

    Dev deploy

--- movies/templatetags/custom_tags.py
+++ movies/templatetags/custom_tags.py
@@ -2,6 +2,7 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter
 def is_moderator(user):
     return user.is_superuser or user.groups.filter(name='Moderators').exists()
